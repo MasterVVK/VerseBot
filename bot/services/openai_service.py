b1b198336acf9aa935741245c65d7b9294c68a2b
MasterVVK <vvk@2140101.ru>
2024-11-29 20:44:00 +0500

    Update openai_service.py

--- bot/services/openai_service.py
+++ bot/services/openai_service.py
@@ -1,22 +1,27 @@
-from openai import OpenAI
 import os
+import asyncio
+from openai import AsyncOpenAI
+from httpx import AsyncClient
+from httpx_socks import AsyncProxyTransport
 from bot.config import OPENAI_API_KEY, PROXY_URL
 
-os.environ["http_proxy"] = PROXY_URL
-os.environ["https_proxy"] = PROXY_URL
 
 class RhymeFinder:
     def __init__(self):
-        # Установка ключа API
-        openai_key = OPENAI_API_KEY
-        # Настройка прокси
-        self.client = OpenAI(api_key=openai_key)
+        # Настройка транспорта для использования SOCKS5-прокси
+        self.transport = AsyncProxyTransport.from_url(PROXY_URL)
+        # Настройка HTTP-клиента через прокси
+        self.client = AsyncOpenAI(
+            api_key=OPENAI_API_KEY,
+            http_client=AsyncClient(transport=self.transport)
+        )
 
     async def get_rhymes(self, word: str):
-        response = self.client.chat.completions.create(
-            model="gpt-4o",
+        # Выполнение асинхронного запроса через OpenAI API
+        response = await self.client.chat.completions.create(
+            model="gpt-4o-2024-08-06",
             messages=[
-                {"role": "system", "content": "Ты помощник, который подбирает рифмы для слов. Твои ответы должны содержать только список слов, разделённых запятыми, без лишнего текста. Не более 5 слов"},
+                {"role": "system", "content": "Ты помощник, который подбирает рифмы для слов. Твои ответы должны содержать только список слов, разделённых запятыми, без лишнего текста. Не более 5 слов."},
                 {"role": "user", "content": f"Подбери рифмы для слова '{word}'."}
             ]
         )
